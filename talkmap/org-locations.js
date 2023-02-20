c2fad0d7ad5cca1f62552dee17828b78abb04886
mbdunn <muriel.bh.dunn@gmail.com>
2023-02-20 17:34:38 +0100

    changes

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "St- John's, Newfoundland, and Labrador Canada",
+    47.5614705,
+    -52.7126162
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Troms\u00f8, Norway",
+    69.6516345,
+    18.9558585
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "St-John's, Canada",
+    47.5614705,
+    -52.7126162
+  ],
+  [
+    "Ny-\u00c5lesund, Svalbard",
+    78.9257958,
+    11.9245516
+  ],
+  [
+    "Hawaii",
+    19.593801499999998,
+    -155.42837009716908
+  ],
+  [
+    "Bergen, Norway",
+    60.3943055,
+    5.3259192
   ]
 ];
